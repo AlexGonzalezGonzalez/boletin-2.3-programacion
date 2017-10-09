554d19b0c4a8ddefe9b7fa0732b38a30ef2d9409
Alejandro gonzalez <>
2017-10-09 09:00:07 +0200

    commit ejercicio 2.3

---
+++ src/boletin/pkg2/pkg1/Boletin21.java
@@ -0,0 +1,14 @@
+
+package boletin.pkg2.pkg1;
+
+public class Boletin21 {
+
+    public static void main(String[] args) {
+        float lado = 3f,area ;
+        area= (float) Math.pow(lado, 2);
+        System.out.println("area = "+ area);
+        
+        
+    }
+    
+}
